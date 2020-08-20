c7f86fe57b9173e96445b52d52d06f6a02bd2244
Robert B <robertb@lifesciencepublishers.com>
2020-08-20 16:21:32 -0600

    add valadation

--- js/script.js
+++ js/script.js
@@ -15,7 +15,34 @@ function closeModal() {
     modal.classList.remove('show-modal');
 }
 
+function storeBookmark(e){
+    e.preventDefault();
+    const nameVlaue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if(!urlValue.includes('http://', 'https://')){
+        urlValue = `https://${urlValue}`;
+    }
+    validate(nameVlaue, urlValue);
+}
+
+function validate(nameVlaue, urlValue){
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if(!nameVlaue || !urlValue){
+        alert('Please submit both fields.');
+        return false
+    }
+    if(!urlValue.match(regex)){
+        alert('Please provide a valid web address');
+        return false;
+    }
+    return true; 
+}
+
 modalShow.addEventListener('click', showModal);
 modleClose.addEventListener('click', closeModal);
-
 window.addEventListener('click', (e)=>{e.target === modal ? closeModal() : false });
+
+
+
+bookmarkForm.addEventListener('submit', storeBookmark)
