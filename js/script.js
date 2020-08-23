2fb85b5cff6419b7ee81c3559d89a6446f538675
Robert Bagwell <erbagwell@yahoo.com>
2020-08-23 14:54:54 -0600

    Add Bookmark js

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@ function closeModal() {
 }
 
 function fetchBookmarks() {
-    if(localStorage.getItem(bookmarks)){
-        bookmarks = JSON.parse(localStorage.getItem(bookmarks))
+    if(localStorage.getItem('bookmarks')){
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     } else {
         bookmarks = [
             {
@@ -27,48 +27,81 @@ function fetchBookmarks() {
                 url: 'https://bob.com'
             }
         ];
-        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
-    console.log(bookmarks);
+    buildBookmarks();
+}
 
+function buildBookmarks() {
+    bookmarksContainer.textContent = '';
+
+    bookmarks.forEach((bookmark)=>{
+        const {name, url} = bookmark;
+
+        const item = document.createElement('div');
+        item.classList.add('item')
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas', 'fa-times');
+        closeIcon.setAttribute('title', 'Delete Bookmark');
+        closeIcon.setAttribute('onclick', `deletBookmark('${url}')`);
+
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `favicon.png`);
+        favicon.setAttribute('alt', 'Favicon');
+
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}` );
+        link.setAttribute('traget', '_blank');
+        link.textContent = name;
+        
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.appendChild(item);
+    });
 }
 
 function storeBookmark(e){
-    e.preventDefault();
-    const nameVlaue = websiteNameEl.value;
-    let urlValue = websiteUrlEl.value;
-    if(!urlValue.includes('http://', 'https://')){
-        urlValue = `https://${urlValue}`;
-    }
-    if(!validate(nameVlaue, urlValue)){
-        return false;
-    }
-
-    const bookmark = {
-        name: nameVlaue,
-        url: urlValue
-    }
-
-    bookmarks.push(bookmark);
-    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
-    // fetchBookmarks();
-    bookmarkForm.reset();
-    websiteNameEl.focus();
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes('http://', 'https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  // Validate
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+  // Set bookmark object, add to array
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  // Set bookmarks in localStorage, fetch, reset input fields
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
-function validate(nameVlaue, urlValue){
-    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+function validate(nameValue, urlValue) {
+    const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
     const regex = new RegExp(expression);
-    if(!nameVlaue || !urlValue){
-        alert('Please submit both fields.');
-        return false
+    if (!nameValue || !urlValue) {
+      alert('Please submit values for both fields.');
+      return false;
     }
-    if(!urlValue.match(regex)){
-        alert('Please provide a valid web address');
-        return false;
+    if (!urlValue.match(regex)) {
+      alert('Please provide a valid web address.');
+      return false;
     }
-    return true; 
-}
+    // Valid
+    return true;
+  }
+  
 
 modalShow.addEventListener('click', showModal);
 modleClose.addEventListener('click', closeModal);
@@ -76,6 +109,6 @@ window.addEventListener('click', (e)=>{e.target === modal ? closeModal() : false
 
 
 
-bookmarkForm.addEventListener('submit', storeBookmark)
+bookmarkForm.addEventListener('submit', storeBookmark);
 
 fetchBookmarks();
